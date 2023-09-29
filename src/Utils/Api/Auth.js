83adcd73e7c00b76d9a1286eb4a8781fda3beb96
yonatan12121 <yonatanmekonnen59@gmail.com>
2023-09-29 12:19:13 +0300

    las

--- src/Utils/Api/Auth.js
+++ src/Utils/Api/Auth.js
@@ -1,10 +1,11 @@
 export const Register = async (data) => {
     console.log(data);
-    const useData = await axios.post("http://localhost:5000/users/register", {
-      data,
-      ContentType: "application/json",
-      Accept: "application/json",
-    });
+    const useData = await axios
+    .post("http://localhost:5000/users/register", data, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
     console.log(useData);
     if (useData.data.status === 201) {
       window.location.href = "/login";
