f141600500e74cfadd5ae0c09d72065a90328d68
yonatan12121 <yonatanmekonnen59@gmail.com>
2023-09-29 15:33:29 +0300

    push

--- src/Utils/Api/Auth.js
+++ src/Utils/Api/Auth.js
@@ -13,3 +13,20 @@ export const Register = async (data) => {
       window.location.href = "/register"
     }
   };
+
+
+  export const Login = async (data) => {
+    console.log(data);
+    const useData = await axios
+    .post("http://localhost:5000/login", data, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+    console.log(useData);
+    if (useData.data.status === 201) {
+      window.location.href = "/login";
+    }else{
+      window.location.href = "/register"
+    }
+  };
