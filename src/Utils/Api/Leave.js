471cb8bfa068091c94f1c560124ab825cdb6b82e
beka-tegen <bekategene177@gmail.com>
2023-09-29 17:53:32 +0300

    modify

--- src/Utils/Api/Leave.js
+++ src/Utils/Api/Leave.js
@@ -1,21 +1,20 @@
 import axios from "axios";
 
-
 export const Request = async (data) => {
-    console.log(data);
-    const useData = await axios.post(
-      "http://localhost:5000/leave/request",
-      data,
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      }
-    );
-    console.log(useData);
-    if (useData.status === 201) {
-      window.location.href = "/";
-    } else {
-      window.location.href = "/register";
+  console.log(data);
+  const useData = await axios.post(
+    "http://localhost:5000/leave/request",
+    data,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
     }
-  };
+  );
+  console.log(useData);
+  if (useData.status === 201) {
+    window.location.href = "/employer-dashboard";
+  } else {
+    alert("error");
+  }
+};
