0cda538cf4f14b240d9deda6aef11a6ed197c0ef
beka-tegen <bekategene177@gmail.com>
2023-10-05 11:08:41 +0300

    modify

--- src/Components/Employer/Profile.jsx
+++ src/Components/Employer/Profile.jsx
@@ -200,7 +200,7 @@ const Profile = () => {
             </Stack>
           )}
           <Typography>
-            <b>ID : </b> {decodedToken.date.Id}
+            <b>ID : </b> {decodedToken.data.Id}
           </Typography>
           <Typography>
             <b>Name : </b> {decodedToken?.data?.name}
